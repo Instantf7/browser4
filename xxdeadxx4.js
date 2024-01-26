39e1280f59fc975ae75511de8e32e1abfcb674ad
Instantf7 <138810527+Instantf7@users.noreply.github.com>
2024-01-25 18:08:20 -0800

    Update and rename xxdeadxx3.js to xxdeadxx4.js

---
+++ xxdeadxx4.js
@@ -0,0 +1,18 @@
+(function() {
+    // Store the initial load time
+    const initialLoadTime = localStorage.getItem('initialLoadTime');
+
+    // Check if the initial load time is set and if the current time is more than 6 seconds from the initial load time
+    if (initialLoadTime && (new Date().getTime() - initialLoadTime) > 6000) {
+        // Redirect if the page is refreshed after 6 seconds
+        window.location.href = '/x444instant-blockedx444';
+    } else {
+        // Set the initial load time if not set
+        localStorage.setItem('initialLoadTime', new Date().getTime());
+    }
+
+    // Clear the stored time on page unload so it resets on navigation to a new page
+    window.addEventListener('beforeunload', function() {
+        localStorage.removeItem('initialLoadTime');
+    });
+})();
